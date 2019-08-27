706224da67a23c872b52be9d6d6a679c891e5778
Jeffrey Paul <jp@eeqj.com>
2019-08-27 18:14:38 +0200

    version bump 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sanelogging',
-    version='1.0.1',
+    version='1.1.0',
     author='Jeffrey Paul',
     author_email='sneak@sneak.berlin',
     packages=['sanelogging'],
